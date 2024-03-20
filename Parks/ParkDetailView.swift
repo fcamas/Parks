63b353786823c838410700cbe219786f2060c4a0
fcamas <fredycamasdev@gmail.com>
2024-03-20 14:02:00 -0400

    added park detail view

---
+++ Parks/ParkDetailView.swift
@@ -0,0 +1,17 @@
+//
+//  ParkDetailView.swift
+//  Parks
+//
+//  Created by Fredy Camas on 4/1/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct ParkDetailView: View {
+    
+    let park: Park // <-- park property to allow for passing in a park when the detail is presented
+    var body: some View {
+        Text("hello")
+    }
+}
